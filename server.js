a46efe54c0f788385c0d0d232c672274d818c538
mikeslerner@gmail.com <mikeslerner@gmail.com>
2018-09-16 19:28:43 -0600

    added hbs partials

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('Home Page');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the site',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
