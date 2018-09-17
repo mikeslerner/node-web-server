aa2d859e2a4f6621f2a4f66941a4e42760422adf
mikeslerner@gmail.com <mikeslerner@gmail.com>
2018-09-16 21:00:40 -0600

    added projects page

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    message: 'My portfolio page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request.'
